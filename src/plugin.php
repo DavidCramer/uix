27834422d075ab4a132a37005087801c479da924
David Cramer <David Cramer>
2016-02-18 15:33:30 +0200

    plugin template

---
+++ src/plugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   {{namespace}}
+ * @author    {{author}}
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2016 {{author}}
+ *
+ * @wordpress-plugin
+ * Plugin Name: {{name}}
+ * Plugin URI:  {{url}}
+ * Description: {{description}}
+ * Version:     1.0.0
+ * Author:      {{author}}
+ * Author URI:  {{url}}
+ * Text Domain: {{namespace}}
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define('{{SLUG}}_PATH',  plugin_dir_path( __FILE__ ) );
+define('{{SLUG}}_CORE',  __FILE__ );
+define('{{SLUG}}_URL',  plugin_dir_url( __FILE__ ) );
+define('{{SLUG}}_VER',  '1.0.0' );
+
+// Load instance
+add_action( 'plugins_loaded', function(){
+	// include the library
+	include_once {{SLUG}}_PATH . 'classes/uix.php';
+	// front
+	if( !is_admin() ){
+		// front class
+		include_once {{SLUG}}_PATH . 'classes/front.php';
+	}
+	
+	// get the pages
+	$pages = include {{SLUG}}_PATH . 'includes/pages.php';
+
+	// initialize admin UI
+	$uix = \{{namespace}}\ui\uix::get_instance( $pages, '{{namespace}}' );
+	$uix->register_pages( $pages );
+
+	//$metaboxes = include {{namespace}}_PATH . 'includes/metaboxes.php';	
+	//$uix->register_metaboxes( $metaboxes );	
+	
+} );
