f21446957ac7233ff68f9da2b5c36c237a278abb
David Cramer <david@digilab.co.za>
2016-03-21 10:32:10 +0200

    files only

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function (grunt) {
         copy: {
             main: {
                 expand: true,
-                src: 'src/assets/css/fonts/',
+                src: 'src/assets/css/fonts/*',
                 dest: 'assets/css/fonts/',
             }
         },        
