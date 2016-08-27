4ca6e12d69aec722c464b89b3b072bd6df6e5405
David Cramer <david@digilab.co.za>
2016-08-27 11:56:06 +0200

    add starter plugin

---
+++ src/plugin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package   uixv2
+ * @author    %author%
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2016 %author%
+ *
+ * @wordpress-plugin
+ * Plugin Name: %name%
+ * Plugin URI:  %url%
+ * Description: %description%
+ * Version:     %version%
+ * Author:      %author%
+ * Author URI:  %url%
+ * Text Domain: text-domain
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define('UIXV2_PATH',  plugin_dir_path( __FILE__ ) );
+define('UIXV2_CORE',  __FILE__ );
+define('UIXV2_URL',  plugin_dir_url( __FILE__ ) );
+define('UIXV2_VER',  '1.0.0' );
+
+/**
+ * locate and find class via classes folder tructure.
+ *
+ * @since 2.0.0
+ *
+ * @param string $class     class name to be checked and autoloaded
+ */
+function uixv2_autoload_class( $class ){
+    $parts = explode( '\\', $class );
+    $name = array_shift( $parts );
+    if( file_exists( UIXV2_PATH . 'classes/' . $name ) ){        
+        $class_file = UIXV2_PATH . 'classes/' . $name . '/' . implode( '/', $parts ) . '.php';
+        if( file_exists( $class_file ) ){
+            include_once $class_file;
+        }
+    }
+}
+spl_autoload_register( 'uixv2_autoload_class', true, false );
+
+// bootstrap plugin load
+add_action( 'plugins_loaded', 'uixv2_plugin_bootstrap', 200 );
+function uixv2_plugin_bootstrap(){
+
+	// start DB Post-Types UI
+	new \uixv2\ui();
+	new \uixv2\core();
+
+	
+}
+
