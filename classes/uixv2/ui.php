e7eb311c85f1a92b34970c477d2e18f36ebca01d
David Cramer <david@digilab.co.za>
2016-08-30 16:16:26 +0200

    register autoloader

--- classes/uixv2/ui.php
+++ classes/uixv2/ui.php
@@ -17,47 +17,85 @@ namespace uixv2;
  */
 class ui{
 
+
+    /**
+     * Array of definitions locations
+     *
+     * @since 1.0.0
+     *
+     * @var   array
+     */
+    protected $locations = array();
+
 	/**
 	 * UI constructor
 	 *
 	 * @since 2.0.0
 	 * @param array $locations array of loader locations and callbacks
 	 */
-	public function __construct( array $locations = array() ) {
+	public function __construct() {
 
 		/**
-		 * Filter UI loader locations
+		 * do UI loader locations
 		 *
-		 * @param array $locations array of core UIX object structures to be registered
+		 * @param uixv2/ui $current UI object
 		 */
-		$locations = apply_filters( 'uixv2_ui_locations', $locations );
+		do_action( 'uixv2_register', $this );
 
 		// go over each locations
-		foreach( $locations as $location ){
-			if( empty( $location['dir'] ) || empty( $location['callback'] ) ){ continue; }
-			$ui_dir = $location['dir'];
-			$uid = @ opendir( $ui_dir );
-			$structures = array();
+		foreach( $this->locations as $location ){
+			$uid = @ opendir( $location );
 			if ( $uid ) {
-				while (($file = readdir( $uid ) ) !== false ) {
-					if ( substr($file, 0, 1) == '.' )
+				while( ( $folder = readdir( $uid ) ) !== false ) {
+					if ( substr( $folder, 0, 1) == '.' )
 						continue;
-					if ( is_file( $ui_dir . '/' . $file ) ) {
-						$is_struct = include $ui_dir . '/' . $file;
-						if( is_array( $is_struct ) ){
-							$structures = array_merge( $structures, $is_struct );
+
+					if ( is_dir( $location . '/' . $folder ) ) {
+						$fid = @ opendir( $location . '/' . $folder );
+						if ( $fid ) {
+							$init = array( '\uixv2\ui\\' . $folder, 'register' );
+							if( !is_callable( $init ) ){
+								continue;
+							}
+							$structures = array();
+							while( ( $file = readdir( $fid ) ) !== false ) {
+								if( is_file( $location . '/' . $folder . '/' . $file ) ){
+									$is_struct = include $location . '/' . $folder . '/' . $file;
+									if( is_array( $is_struct ) ){
+										$structures = array_merge( $structures, $is_struct );
+									}
+								}
+							}
+							if( !empty( $structures ) ){
+								call_user_func_array( $init, array( $structures ) );
+							}
 						}
 					}
 				}
 				@closedir( $uid );
 			}
-
-			if( !empty( $structures ) ){
-				call_user_func_array( $location['callback'], array( $structures ) );
-			}
-
-		}		
-
+		}
 	}
+
+
+    /**
+     * Register the UIX objects
+     *
+     * @since 1.0.0
+     *
+     * @param array|string $args path or array of paths to structures
+     */
+    public function register( $arr ) {
+    	// convert if string
+    	if( is_string( $arr ) ){
+    		$arr = array( $arr );
+    	}
+    	// determin how the structure works.
+    	foreach( $arr as $key => $value ){
+			if( is_dir( $value ) && !in_array( $value, $this->locations ) ){
+				$this->locations[] = $value;
+			}
+    	}
+    }
 
 }
