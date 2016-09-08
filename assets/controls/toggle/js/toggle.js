a9c2c6aa1b1d84dfe67f56b28264cb6cd316ecd2
David Cramer <david@digilab.co.za>
2016-09-08 18:37:08 +0200

    toggle all options

--- assets/controls/toggle/js/toggle.js
+++ assets/controls/toggle/js/toggle.js
@@ -3,17 +3,38 @@
     jQuery( function( $ ){
         $( document ).on( 'change', '.uix-control .switch', function( e ){
             var clicked     = $( this ),
+                parent      = clicked.closest( '.uix-section-content' ),                
+                toggleAll   = parent.find( '[data-toggle-all="true"]' ),
+                allcount    = parent.find( '.uix-control .switch > input' ).not( toggleAll ).length,
+                tottlecount = parent.find( '.uix-control .switch > input:checked' ).not( toggleAll ).length,
                 control     = $( '#' + clicked.data('for') );
 
             if( control.is(':checked') ){
-                clicked.addClass( 'active' );                          
+                clicked.addClass( 'active' );
+                if( allcount === tottlecount ){
+                   toggleAll.prop( 'checked', true ).parent().addClass( 'active' );
+                }
+
             }else{
                 clicked.removeClass( 'active' );
+                if( toggleAll.length ){
+                    toggleAll.prop( 'checked', false ).parent().removeClass( 'active' );
+                }
             }
 
         } );
 
         $( '.uix-control .switch' ).trigger( 'change' );
+
+        $( document ).on('change', '[data-toggle-all="true"]', function(e){
+            var clicked = $( this ),
+                parent = clicked.closest( '.uix-section-content' );
+
+            parent.find('.uix-control .switch > input').not( this ).prop('checked', this.checked ).trigger('change');
+        });
+
     });
+
+
 
 })( jQuery );
