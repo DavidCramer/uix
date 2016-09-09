80ce4923f705c05cfc26fb02dd53f180615b6bd0
David Cramer <david@digilab.co.za>
2016-09-09 10:47:49 +0200

    cleanups

--- uix-bootstrap.php
+++ uix-bootstrap.php
@@ -21,21 +21,22 @@
  * Domain Path: /languages
  */
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-    die;
+if ( defined( 'WPINC' ) ) {
+
+    if( ! defined( 'UIX_CORE' ) ){
+        define('UIX_CORE',  __FILE__ );
+        define('UIX_PATH',  plugin_dir_path( __FILE__ ) );
+        define('UIX_URL',  plugin_dir_url( __FILE__ ) );
+        define('UIX_VER',  '1.0.0' );
+    }
+
+    // include uix helper functions and autoloader.
+    require_once( UIX_PATH . 'includes/functions.php' );
+
+    // register uix autoloader
+    spl_autoload_register( 'uix_autoload_class', true, false );
+
+    // bootstrap plugin load
+    add_action( 'plugins_loaded', 'uix' );
+
 }
-if( ! defined( 'UIX_CORE' ) ){
-    define('UIX_CORE',  __FILE__ );
-    define('UIX_PATH',  plugin_dir_path( __FILE__ ) );
-    define('UIX_URL',  plugin_dir_url( __FILE__ ) );
-    define('UIX_VER',  '1.0.0' );
-}
-
-// include uix helper functions and autoloader.
-require_once( UIX_PATH . 'includes/functions.php' );
-
-// register uix autoloader
-spl_autoload_register( 'uix_autoload_class', true, false );
-
-// bootstrap plugin load
-add_action( 'plugins_loaded', 'uix' );
