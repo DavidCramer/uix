21e53733309ed24a55af80957bed86764715f473
David Cramer <david@digilab.co.za>
2016-09-14 07:21:50 +0200

    installer fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,12 @@
                 ]
             }
         },
+        rename: {
+            core: {
+                src: '../classes/<%= pkg.namespace %>/ui/uix.php',
+                dest: '../classes/<%= pkg.namespace %>/ui/<%= pkg.namespace %>.php'
+            }
+        },
         uglify: {
             min: {
                 files: grunt.file.expandMapping( [
@@ -70,19 +76,19 @@
             }
         },
         clean: {
-          build: ["etc/**", "node_modules/**",".git/**",".gitignore","composer.json","Gruntfile.js","package.json"],
+            build: ["etc/**", "node_modules/**",".git/**",".gitignore","composer.json","Gruntfile.js","package.json"],
         },
 
     });
 
-    //load modules
-    grunt.loadNpmTasks( 'grunt-curl' );
-    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+     //load modules
+     grunt.loadNpmTasks( 'grunt-curl' );
+     grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+     grunt.loadNpmTasks( 'grunt-contrib-copy' );
+     grunt.loadNpmTasks( 'grunt-contrib-clean' );
+     grunt.loadNpmTasks( 'grunt-rename' );
+     //installer tasks
+     grunt.registerTask( 'default', [ 'curl', 'copy', 'rename', 'cssmin', 'uglify', 'clean' ] );
 
-    //installer tasks
-    grunt.registerTask( 'default', [ 'curl', 'copy', 'cssmin', 'uglify', 'clean' ] );
-
-};
+ };
