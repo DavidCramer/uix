5f3819deb3cb3622e15439e7a589efc3cc8c0ba6
David Cramer <david@digilab.co.za>
2016-10-20 07:37:33 +0200

    add UIX plugin over bootstrapper - fixes #7 ... again

--- uix-bootstrap.php
+++ uix-bootstrap.php
@@ -8,17 +8,6 @@
  * @link
  * @copyright 2016 David Cramer
  *
- * @wordpress-plugin
- * Plugin Name: UIX
- * Plugin URI:  http://cramer.co.za
- * Description: UI Framework for WordPress Plugins.
- * Version:     1.0.0
- * Author:      David Cramer
- * Author URI:  http://cramer.co.za
- * Text Domain: uix
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /languages
  */
 // If this file is called directly, abort.
 if ( defined( 'WPINC' ) ) {
