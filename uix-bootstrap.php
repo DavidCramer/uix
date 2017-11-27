2ff45c50b6c594c8e429f2fcf1ed1397ec27b745
David Cramer <david@digilab.co.za>
2017-11-27 15:48:13 +0200

    clean up boilerplate

--- uix-bootstrap.php
+++ uix-bootstrap.php
@@ -6,9 +6,9 @@
  * @author    David Cramer
  * @license   GPL-2.0+
  * @link
- * @copyright 2016 David Cramer
- *
+ * @copyright 2017 David Cramer
  */
+
 // If this file is called directly, abort.
 if ( defined( 'WPINC' ) ) {
 
@@ -28,12 +28,12 @@ if ( defined( 'WPINC' ) ) {
 
 
 	// include uix helper functions and autoloader.
-	require_once( UIX_PATH . 'includes/functions.php' );
+	require_once UIX_PATH . 'includes/functions.php';
 
-	// register uix autoloader
+	// register uix autoloader.
 	spl_autoload_register( 'uix_autoload_class', true, false );
 
-	// bootstrap plugin load
+	// bootstrap plugin load.
 	add_action( 'plugins_loaded', 'uix' );
 
 }
