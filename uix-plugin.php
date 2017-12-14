8ca29040414cfd7c9e49960a7e86925f1ea9b59c
David Cramer <david@digilab.co.za>
2017-12-14 13:32:06 +0200

    set version

--- uix-plugin.php
+++ uix-plugin.php
@@ -7,12 +7,11 @@
  * @license   GPL-2.0+
  * @link
  * @copyright 2016 David Cramer
- *
  * @wordpress-plugin
  * Plugin Name: UIX
  * Plugin URI:  http://cramer.co.za
  * Description: UI Framework for WordPress Plugins.
- * Version:     1.0.0
+ * Version:     3.0.0
  * Author:      David Cramer
  * Author URI:  http://cramer.co.za
  * Text Domain: uix
@@ -20,13 +19,14 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path: /languages
  */
+
 // If this file is called directly, abort.
 if ( defined( 'WPINC' ) ) {
 
 	define( 'UIX_CORE', __FILE__ );
 	define( 'UIX_PATH', plugin_dir_path( __FILE__ ) );
 	define( 'UIX_URL', plugin_dir_url( __FILE__ ) );
-	define( 'UIX_VER', '1.0.0' );
+	define( 'UIX_VER', '3.0.0' );
 
 	// include uix bootstrap.
 	require_once UIX_PATH . 'uix-bootstrap.php';
