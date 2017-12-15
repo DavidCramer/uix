d0907e3bc71c2a9b5553365e13bbe0f53e871a1f
David Cramer <david@digilab.co.za>
2017-12-15 11:54:45 +0200

    add Image control

---
+++ assets/controls/image/js/image.js
@@ -0,0 +1,53 @@
+(function () {
+
+    jQuery(document).ready(function ($) {
+
+        // Uploading files
+        var file_frame;
+        var field;
+        jQuery(document).on('click', '.uix-image-control-button', function (event) {
+
+            event.preventDefault();
+
+            field = $(this).data('target');
+
+            // If the media frame already exists, reopen it.
+            if (file_frame) {
+                // Open frame
+                file_frame.open();
+                return;
+            }
+
+            // Create the media frame.
+            file_frame = wp.media.frames.file_frame = wp.media({
+                title: 'Select a image to upload',
+                button: {
+                    text: 'Use this image',
+                },
+                multiple: false	// Set to true to allow multiple files to be selected
+            });
+
+            // When an image is selected, run a callback.
+            file_frame.on('select', function () {
+                // We set multiple to false so only get one image from the uploader
+                attachment = file_frame.state().get('selection').first().toJSON();
+
+                // Do something with attachment.id and/or attachment.url here
+                $('#' + field + '-wrap').html('<img src="' + attachment.url + '" class="uix-image-control-preview"><a href="#" class="uix-image-control-remove" data-target="' + field + '"><span class="dashicons dashicons-no"></span></a>');
+                $('#' + field + '-control').val(attachment.id);
+
+            });
+
+            // Finally, open the modal
+            file_frame.open();
+        });
+        jQuery(document).on('click', '.uix-image-control-remove', function (event) {
+            event.preventDefault();
+            var field = $(this).data('target');
+            $('#' + field + '-wrap').html('');
+            $('#' + field + '-control').val('');
+        });
+
+    });
+
+})(window);
---
+++ classes/uix/ui/control/image.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * UIX Image Control
+ *
+ * @package   controls
+ * @author    David Cramer
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2016 David Cramer
+ */
+
+namespace uix\ui\control;
+
+/**
+ * Image picker field
+ *
+ * @since 3.0.0
+ */
+class image extends \uix\ui\control {
+
+	/**
+	 * The type of object
+	 *
+	 * @since       1.0.0
+	 * @access      public
+	 * @var         string
+	 */
+	public $type = 'image';
+
+	/**
+	 * Checks if the current control is active.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function is_active() {
+		$active = parent::is_active();
+
+		if ( true === $active ) {
+			wp_enqueue_media();
+		}
+
+		return $active;
+	}
+
+	/**
+	 * Define core UIX scripts - override to register core ( common scripts for
+	 * uix type )
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 */
+	public function set_assets() {
+
+		$this->assets['style']['image-control']  = $this->url . 'assets/controls/image/css/image' . UIX_ASSET_DEBUG . '.css';
+		$this->assets['script']['image-control'] = [
+			'src'       => $this->url . 'assets/controls/image/js/image' . UIX_ASSET_DEBUG . '.js',
+			'in_footer' => true,
+		];
+		parent::set_assets();
+	}
+
+	/**
+	 * Returns the main input field for rendering
+	 *
+	 * @since  1.0.0
+	 * @see    \uix\ui\uix
+	 * @access public
+	 * @return string Input field HTML string
+	 */
+	public function input() {
+
+		$output      = '<input type="hidden" value="' . esc_attr( $this->get_value() ) . '" ' . $this->build_attributes() . '>';
+		$button_text = isset( $this->struct['add_label'] ) ? $this->struct['add_label'] : __( 'Select Image', 'uix' );
+		$output      .= '<div class="uix-image-control-wrapper" id="' . esc_attr( $this->id() ) . '-wrap">';
+		if ( null !== $this->get_value() ) {
+			$output .= $this->get_preview();
+		}
+		$output .= '</div>';
+		$output .= '<button type="button" data-target="' . esc_attr( $this->id() ) . '" class="button button-small uix-image-control-button">' . esc_html( $button_text ) . '</button>';
+
+		return $output;
+	}
+
+	/**
+	 * Render the Control preview.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string HTML of rendered preview
+	 */
+	private function get_preview() {
+		$data   = wp_get_attachment_image_src( $this->get_value(), 'medium' );
+		$return = null;
+		if ( ! empty( $data ) ) {
+			$return = '<img src="' . esc_attr( $data[0] ) . '" class="uix-image-control-preview"><a href="#" class="uix-image-control-remove" data-target="' . $this->id() . '"><span class="dashicons dashicons-no"></span></a>';
+		}
+
+		return $return;
+
+	}
+}
